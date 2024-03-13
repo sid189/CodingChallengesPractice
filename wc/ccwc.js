9652600da212554353b2e4cc3641262340c801fa
Siddharth Deshpande <sid18995@gmail.com>
2024-03-13 16:41:44 -0400

    Add cli options

--- wc/ccwc.js
+++ wc/ccwc.js
@@ -1,16 +1,39 @@
-const { argv } = require('node:process');
-const fs = require('fs')
+#!/usr/bin/env node
+
+import { argv } from 'node:process';
+import { readFile } from 'fs';
 const byteSize = str => new Blob([str]).size;
 
-console.log(argv[2])
-console.log(argv[3])
 
-fs.readFile('Input.txt', (err, data) => {
+readFile(argv[3], (err, data) => {
   if (err) throw err;
- 
-  console.log(data.toString());
-  console.log(byteSize(data.toString()));
-  console.log(data.toString().split('\n').length-1);
-  console.log(data.toString().trim().split(/\s+/).length);
-  console.log(data.toString().length);
+
+
+
+  if (argv[2] == "-c") {
+    console.log((byteSize(data.toString())) + " " + (argv[3]));
+  }
+  
+  else if (argv[2] == "-l") {
+    console.log(data.toString().split('\n').length-1 + " " + (argv[3]));
+  }
+  
+  else if (argv[2] == "-w") {
+    console.log(data.toString().trim().split(/\s+/).length + " " + (argv[3]));
+  }
+  
+  else if (argv[2] == "-m") {
+    console.log(data.toString().length + " " + (argv[3]));
+  }
+
+  else if (argv.length == 3 && argv[2] == "Input.txt") {
+    console.log(data.toString().split('\n').length-1 + " " 
+    + data.toString().trim().split(/\s+/).length + " "
+    + (byteSize(data.toString())) + " "
+    + argv[2])
+  }
+
+  else {
+    console.log("Pass the correct parameters")
+  }
 });
